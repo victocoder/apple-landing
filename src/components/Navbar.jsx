b9af53ebc2661bb622006263da2466efb082600c
Mesfin Girma <mesfingirmatwo@gmail.com>
2025-03-20 15:24:48 +0300

    styled nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
-import { appleImg } from '../utils'
+import { appleImg, bagImg } from '../utils'
 import { searchImg } from '../utils'
+import { navLists } from '../constants'
 const Navbar = () => {
   return (
-    <header>
-        <nav>
+    <header className='w-full flex justify-between items-center py-5 px-5  sm:px-10'>
+        <nav className='flex w-full screen-max-width'>
             <img src={appleImg} alt="apple" width={14} height={18} />
 
-            <div>
+            <div className='flex flex-1 justify-center max-sm:hidden'>
                 {
-                    ['Phone', 'Macbooks','Tablets'].map((nav)=>(
-                        <div key={nav}>
+                    navLists.map((nav)=>(
+                        <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
                           {nav}
                         </div>
                     ))
                 }
+            </div>
+            <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
+            <img src={searchImg} alt="search" width={14} height={18} />
+            <img src={bagImg} alt="bag" width={14} height={18} />
+
             </div>
         </nav>
     </header>
